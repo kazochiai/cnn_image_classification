29c63087eacd702d93380f116e8034a996e96598
kochiai <kochiai@CHIC02SY0PHH03Q.grubhub.local>
2017-09-16 12:17:19 -0400

    first commit

---
+++ download_images.py
@@ -0,0 +1,40 @@
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+from urllib.request import urlretrieve
+from os.path import isfile, isdir
+from tqdm import tqdm
+import unit_tests.problem_unittests as tests
+import tarfile
+
+cifar10_dataset_folder_path = 'cifar-10-batches-py'
+
+# Use Floyd's cifar-10 dataset if present
+floyd_cifar10_location = '/cifar/cifar-10-python.tar.gz'
+if isfile(floyd_cifar10_location):
+    tar_gz_path = floyd_cifar10_location
+else:
+    tar_gz_path = 'cifar-10-python.tar.gz'
+
+class DLProgress(tqdm):
+    last_block = 0
+
+    def hook(self, block_num=1, block_size=1, total_size=None):
+        self.total = total_size
+        self.update((block_num - self.last_block) * block_size)
+        self.last_block = block_num
+
+if not isfile(tar_gz_path):
+    with DLProgress(unit='B', unit_scale=True, miniters=1, desc='CIFAR-10 Dataset') as pbar:
+        urlretrieve(
+            'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
+            tar_gz_path,
+            pbar.hook)
+
+if not isdir(cifar10_dataset_folder_path):
+    with tarfile.open(tar_gz_path) as tar:
+        tar.extractall()
+        tar.close()
+
+
+tests.test_folder_path(cifar10_dataset_folder_path)
